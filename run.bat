fd4d4eef234fec1db2eb4f6882772b55e2640718
elijah-nelson <elipoplefish@gmail.com>
2024-10-02 20:13:59 +1000

    Properly escape git pull output

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 setlocal enabledelayedexpansion
 
-:: if .venv directory doesn't exist, create venv
+:: if .venv directory doesn't exist, create venv and update virtual environment
 set "found_venv=0"
 for /f "delims=" %%a in ('dir /b /AD') do (
     echo %%a | findstr /i ".venv" >nul
@@ -10,19 +10,14 @@ for /f "delims=" %%a in ('dir /b /AD') do (
     )
 )
 :break_loop1
-if %found_venv%=="0" python -m venv .venv
+if %found_venv%=="0" (
+    python -m venv .venv
+    .\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
+)
 
 :: if git pull updates requirements.txt, update virtual environment
 for /f "delims=" %%a in ('git pull') do (
-    set "line=%%a"
-    :: Escape special characters like |, &, <, >, etc.
-    set "line=!line:^|=^|!"
-    set "line=!line:^&=^&!"
-    set "line=!line:^<=^<!"
-    set "line=!line:^>=^>!"
-    set "line=!line:^!=^^!"
-
-    echo !line! | findstr /i "requirements.txt" >nul
+    echo "%%a" | findstr /i "requirements.txt" >nul
     if not errorlevel 1 (
         .\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
         goto :break_loop2
