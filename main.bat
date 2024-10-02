89957c72f561f5c47c57508508ac674e123b22c6
elijah-nelson <elipoplefish@gmail.com>
2024-10-02 20:20:25 +1000

    Seperate run.bat into main.bat to allow for updates to the script to happen before its ran

---
+++ main.bat
@@ -0,0 +1,23 @@
+setlocal enabledelayedexpansion
+
+:: if .venv directory doesn't exist, create venv
+set "found_venv=0"
+for /f "delims=" %%a in ('dir /b /AD') do (
+    echo %%a | findstr /i ".venv" >nul
+    if not errorlevel 1 (
+        set "found_venv=1"
+        goto :break_loop1
+    )
+)
+:break_loop1
+if %found_venv%=="0" (
+    python -m venv .venv
+)
+
+:: always update virtual environment since the script isn't ran very often
+.\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
+
+:: run main.py
+.\.venv\Scripts\python.exe main.py
+
+pause
--- run.bat
+++ run.bat
@@ -1,23 +1,5 @@
-setlocal enabledelayedexpansion
+setlocal
 
-:: if .venv directory doesn't exist, create venv
-set "found_venv=0"
-for /f "delims=" %%a in ('dir /b /AD') do (
-    echo %%a | findstr /i ".venv" >nul
-    if not errorlevel 1 (
-        set "found_venv=1"
-        goto :break_loop1
-    )
-)
-:break_loop1
-if %found_venv%=="0" (
-    python -m venv .venv
-)
-
+:: seperate file to main.bat so that any changes to main.bat get loaded before its executed
 git pull
-:: always update virtual environment since the script isn't ran very often
-.\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
-
-:: run main.py
-.\.venv\Scripts\python.exe main.py
-pause
+.\main.bat
