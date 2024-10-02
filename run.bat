78c7302f859aa52c15498b341c81d9c902eba7cf
elijah-nelson <elipoplefish@gmail.com>
2024-10-02 20:00:56 +1000

    Fix run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,27 @@
-@REM store list of directories in current directory in %FILES%
-FOR /F "tokens=*" %%g IN ('dir /b /AD') do (SET FILES=%%g)
-@REM if %FILES% doesn't contains the string .venv, create virtual environment
-if x%FILES:.venv=%==x%FILES% python -m venv .venv
-@REM store output from git pull in %GIT_PULL%
-FOR /F "tokens=*" %%g IN ('git pull') do (SET GIT_PULL=%%g)
-@REM if %GIT_PULL% contains the string requirements.txt, update virtual environment
-if not x%GIT_PULL:requirements.txt=%==x%GIT_PULL% .\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
+setlocal
+
+:: if .venv directory doesn't exist, create venv
+set "found_venv=0"
+for /f "delims=" %%a in ('dir /b /AD') do (
+    echo %%a | findstr /i ".venv" >nul
+    if not errorlevel 1 (
+        set "found_venv=1"
+        goto :break_loop1
+    )
+)
+:break_loop1
+if %found_venv%=="0" python -m venv .venv
+
+:: if git pull updates requirements.txt, update virtual environment
+for /f "delims=" %%a in ('git pull') do (
+    echo %%a | findstr /i "requirements.txt" >nul
+    if not errorlevel 1 (
+        .\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
+        goto :break_loop2
+    )
+)
+:break_loop2
+
+:: run main.py
 .\.venv\Scripts\python.exe main.py
 pause
