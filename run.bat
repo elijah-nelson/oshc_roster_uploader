7ee440dd3c573f07faa31cd0d59ef94365944f90
elijah-nelson <elipoplefish@gmail.com>
2024-10-02 20:15:37 +1000

    Do the boring option of always updating python packages

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 setlocal enabledelayedexpansion
 
-:: if .venv directory doesn't exist, create venv and update virtual environment
+:: if .venv directory doesn't exist, create venv
 set "found_venv=0"
 for /f "delims=" %%a in ('dir /b /AD') do (
     echo %%a | findstr /i ".venv" >nul
@@ -12,18 +12,11 @@ for /f "delims=" %%a in ('dir /b /AD') do (
 :break_loop1
 if %found_venv%=="0" (
     python -m venv .venv
-    .\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
 )
 
-:: if git pull updates requirements.txt, update virtual environment
-for /f "delims=" %%a in ('git pull') do (
-    echo "%%a" | findstr /i "requirements.txt" >nul
-    if not errorlevel 1 (
-        .\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
-        goto :break_loop2
-    )
-)
-:break_loop2
+git pull
+:: always update virtual environment since the script isn't ran very often
+.\.venv\Scripts\pip.exe install --upgrade -r requirements.txt
 
 :: run main.py
 .\.venv\Scripts\python.exe main.py
